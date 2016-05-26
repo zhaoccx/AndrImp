b9de817dd0c89a2046ec483b1f4b1103bcd4f0ea
趙常春 <zhaoccx@hotmail.com>
2016-05-26 10:01:52 +0800

    init AndrImp

---
+++ ReadXmlByPull/src/com/leequer/Service/Test.java
@@ -0,0 +1,27 @@
+package com.leequer.Service;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.leequer.Doem.Person;
+
+public class Test extends AndroidTestCase {
+	private String PERSONSTRING = "ObjectPerson";
+
+	public void testReadXml() throws Exception {// Àà×°ÔØÆ÷
+		InputStream inputStream = Test.class.getClassLoader().getResourceAsStream("NewFile.xml");
+
+		List<Person> personsList = ReadXmlByPullService.ReadXmlByPull(inputStream);
+		Log.e("abc", personsList.get(0).getName());
+
+		for (Iterator<Person> iterator = personsList.iterator(); iterator.hasNext();) {
+			Person person = (Person) iterator.next();
+			Log.i(PERSONSTRING, person.toString());
+		}
+
+	}
+}
